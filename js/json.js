a5d6e50d7aa29b55405e8f2dfd18646296d244f8
EricMareth <EricMareth@fullsail.edu>
2012-12-05 02:31:44 -0500

    Final week, let's finish this puppy.

---
+++ js/json.js
@@ -0,0 +1,30 @@
+// Web App Part 4
+// Eric Mareth
+// Visual Frameworks 1212
+
+
+var json = {
+	"character1" : {
+		"name" :	["Name:", "Fuzzi"],
+		"story":	["Story:", "Fuzzi's Day Out"],
+		"land":		["Land:", "FuzziLand"],
+		"town":		["Town:", "FuzzCentral"],
+		"gender":	["Sex:", "It's Complicated"],
+		"age":		["Age:", "150"],
+		"type":		["Character Type:", "Hero"],
+		"details":	["Details:", "Our very fuzzy hero."],
+		"created":	["Birthdate:", "2012-11-12"]
+	},
+	
+	"character2" : {
+		"name" :	["Name:", "Danala"],
+		"story":	["Story:", "The Witch Queen"],
+		"land":		["Land:", "Etera"],
+		"town":		["Town:", "Cabra"],
+		"gender":	["Sex:", "Female"],
+		"age":		["Age:", "35"],
+		"type":		["Character Type:", "Villain"],
+		"details":	["Details:", "The wickedest queen in all of Etera."],
+		"created":	["Birthdate:", "2004-06-25"]
+	}
+}
---
+++ js/main.js
@@ -0,0 +1,296 @@
+// Web App Part 4
+// Eric Mareth
+// Visual Frameworks 1212
+
+window.addEventListener("DOMContentLoaded", function(){
+	
+	function $(x){
+		var theElement = document.getElementById(x);
+		return theElement;
+	}
+	
+	function whatType(){
+		var formTag = document.getElementsByTagName("form"),
+			selectLi = $('selectType'),
+			makeSelect = document.createElement('select');
+			makeSelect.setAttribute("id", "type");
+		for(i=0, j=charType.length; i<j; i++){
+			var makeOption = document.createElement('option');
+			var optText = charType[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(makeSelect);
+	}
+	
+	function getSelectedRadio(){
+		var sexChange = document.forms[0].gender;
+		for(i=0; i<sexChange.length; i++){
+			if (sexChange[i].checked){
+				genderVal = sexChange[i].value;
+			}
+		}
+	}
+	
+	function toggleControls(n){
+		switch(n){
+			case "on":
+				$('charForm').style.display = "none";
+				$('clearLink').style.display = "inline";
+				$('displayData').style.display = "none";
+				$('addChar').style.display = "inline";
+				break;
+			case "off":
+				$('charForm').style.display = "block";
+				$('clearLink').style.display = "inline";
+				$('displayData').style.display = "inline";
+				$('addChar').style.display = "none";			
+				$('items').style.display = "none";
+				break;
+			default:
+				return false;
+		}
+	}
+	
+	
+	function getData(){
+		toggleControls("on");
+		if (localStorage.length === 0){
+			alert("There are no characters lurking in the shadows! So some have been conjured for you...");
+			autoFillData();
+		}
+		var makeDiv = document.createElement('div');
+		makeDiv.setAttribute("id","items");
+		var makeList = document.createElement('ul');
+		makeList.setAttribute("id","bioList");					// Id created to attach CSS.
+		makeDiv.appendChild(makeList);
+		document.body.appendChild(makeDiv);
+		$('items').style.display = "display";
+		for( i=0, length=localStorage.length; i<length; i++){
+			var makeLi = document.createElement('li');
+			makeLi.setAttribute("id","bio");					// Id created to attach CSS.
+			var linksLi = document.createElement('li');
+			makeList.appendChild(makeLi);
+			var key = localStorage.key(i);
+			var value = localStorage.getItem(key);
+			var obj = JSON.parse(value);						// ORIGINAL KEY definition = randomly generated number.
+			var makeSubList = document.createElement('ul');
+			makeLi.appendChild(makeSubList);
+			for(var n in obj){
+				var makeSubLi = document.createElement('li');
+				makeSubList.appendChild(makeSubLi);
+				var optSubText = obj[n][0] + " " + obj[n][1];
+				makeSubLi.innerHTML = optSubText;
+				makeSubList.appendChild(linksLi);
+			}
+			makeItemLinks(localStorage.key(i), linksLi);		// passing key to makeItemLinks function.
+		}
+	}
+	
+	function autoFillData(){									// Auto populates a default character.
+		for(var n in json){
+			var id = Math.floor(Math.random()*10000001);
+			localStorage.setItem(id, JSON.stringify(json[n]));
+		}
+	}	
+	
+	// Creates edit and delete links
+	function makeItemLinks(key, linksLi){						// KEY is passed from getData function.
+		var editLink = document.createElement('a');
+		editLink.href = "#";
+		editLink.key = key;
+		var	editText = "Edit Character";
+		editLink.addEventListener("click", editItem);
+		editLink.innerHTML = editText;
+		linksLi.appendChild(editLink);
+		
+		//var breakTag = document.createElement('br');			// Fashioned my tags with CSS.
+		//linksLi.appendChild(breakTag);
+		linksLi.setAttribute("id", "charAdjust");
+		
+		var deleteLink = document.createElement('a');
+		deleteLink.href = "#";
+		deleteLink.key = key;
+		var deleteText ="Delete Character";
+		deleteLink.addEventListener("click", deleteItem);
+		deleteLink.innerHTML = deleteText;
+		linksLi.appendChild(deleteLink);
+	}
+	
+	function editItem(){
+		//Grabs data from item in local storage.
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+		
+		//Shows the form.
+		toggleControls("off");
+		
+		//populates the form fields with current localStorage values.
+		$('charName').value  = item.name[1];
+		$('taleName').value  = item.story[1];
+		$('land').value  = item.land[1];
+		if($('land').value !== ""){
+			$('town').value  = item.town[1];
+			townField();
+		}
+		var radios = document.forms[0].gender;
+		for(i=0; i<radios.length; i++){
+			if(radios[i].value == "Male" && item.gender[1] == "Male"){
+				radios[i].setAttribute("checked","checked");
+			}else if(radios[i].value == "Female" && item.gender[1] == "Female"){
+				radios[i].setAttribute("checked","checked");
+			}else if(radios[i].value == "It's Complicated" && item.gender[1] == "It's Complicated"){
+				radios[i].setAttribute("checked","checked");
+			}
+		}
+		$('age').value  = item.age[1];
+		ageNum();
+		$('type').value  = item.type[1];
+		$('details').value  = item.details[1];
+		$('created').value  = item.created[1];
+		
+		// remove the initial listener from the input 'save contact'
+		save.removeEventListener("click", storeData);
+		$('saveChar').value = "Edit Character";
+		var editSubmit = $('saveChar');
+		// Save the key value established in this function as a property of the editSubmit event
+		//so we can use that value when we save the data we edited.
+		editSubmit.addEventListener("click", validate);
+		editSubmit.key = this.key;
+	}
+	
+	function deleteItem(){
+		var ask = confirm("Are you sure you want to banish this character?"); 
+		if(ask){
+			localStorage.removeItem(this.key);
+			alert("That character has been irrevocably thrown into the void!")
+			window.location.reload();
+		}else{
+			alert("This character has been SPARED! For now...");
+		}
+	}
+	
+	function clearLocal(){
+		if(localStorage.length === 0){
+			alert("There is no data to clear.");
+		}else{
+			var ask = confirm("Are you positive that you want to wipe out your collection?")
+			if(ask){
+				localStorage.clear();
+				alert("All characters have been destroyed!");
+				window.location.reload();
+				return false;
+			}else{
+				alert("That was a close one!")
+			}
+		}
+	}
+	
+	function validate(e){
+		//Define the elements we want to check.
+		var getName = $('charName');
+		var getType = $('type');
+		
+		errMsg.innerHTML = "";
+		getName.style.border = "1px solid black";
+		getType.style.border = "1px solid black";
+		
+		var messageAry = [];
+		
+		if(getName.value === ""){
+			var nameError = "Please give your character a name.";
+			getName.style.border = "1px solid red";
+			messageAry.push(nameError);	
+		}
+		
+		if(getType.value === "|-Choose Character Type-|"){
+			var typeError = "Please select a character type.";
+			getType.style.border = "1px solid red";
+			messageAry.push(typeError);	
+		}
+		
+		if(messageAry.length >= 1){
+			for(i=0, j=messageAry.length; i < j; i++){
+				var txt = document.createElement('li');
+				txt.innerHTML = messageAry[i];
+				errMsg.appendChild(txt);
+			}
+			 e.preventDefault();
+			return false;
+		}else{
+			storeData(this.key);
+		}
+	}
+	
+		
+	
+	function storeData(key){
+		if(!key){
+			var id			= Math.floor(Math.random()*10000001);
+		}else{
+			id = key;
+		};
+		getSelectedRadio();
+		var item		={};
+			item.name		=["Name:", $('charName').value];
+			item.story		=["Story:", $('taleName').value];
+			item.land		=["Land:", $('land').value];
+			item.town		=["Town:", $('town').value];
+			item.gender		=["Sex:", genderVal];
+			item.age		=["Age:", $('age').value];
+			item.type		=["Character Type:", $('type').value];
+			item.details	=["Details:", $('details').value];
+			item.created	=["Birthdate:", $('created').value];
+		
+		localStorage.setItem(id, JSON.stringify(item));
+		alert("It's ALIVE!!!!!!!! ALIIIIVE!!!!!");
+		
+	}
+	
+	function ageNum(){ 										// My function answer to the challenge to add a range display.
+		var	ageVal = ageData.value,
+			field = $('charAge');
+		field.innerHTML = "";
+		field.innerHTML = ageVal;
+	}
+	
+	function townField(){									// Activates 'Town' field for population.
+		var townFld = $('town'),
+			townLbl = $('townTxt'),
+			newTxt = "Town?: ",
+			dfltText = "Town (disabled): ",
+			place = townFld.setAttribute("placeholder", "Oz, Neverland, etc.");			
+		if($('land').value !== ""){
+			townLbl.style.color = "black";
+			townLbl.innerHTML = newTxt;
+			townFld.removeAttribute("disabled", "disabled");
+		}else{
+			townLbl.style.color = "gray";					// returns 'town' field to original state if user deletes 'land' field data.
+			townLbl.innerHTML = dfltText;
+			townFld.setAttribute("disabled", "disabled");
+			townFld.value = "";
+			place;
+		}
+	}
+	
+	
+	var charType = ["|-Choose Character Type-|", "Hero", "Side-kick", "Love Interest", "Mentor", "Villain", "Henchman", "Sub-Villain", "Supporting", "Walk-on", "Off-Screen"];
+	var	errMsg  = $('errors');
+
+	whatType();
+
+	var displayData = $('displayData');
+	displayData.addEventListener("click", getData);
+	var clearLink = $('clearLink');
+	clearLink.addEventListener("click", clearLocal);
+	var save = $('saveChar');
+	save.addEventListener("click", validate);
+	
+	var ageData = $('age');									// My var answer to the challenge to add a range display.
+	ageNum();
+	ageData.addEventListener("change", ageNum);	
+	var askTown = $('land');								// Checks 'Land' field to determine active state of townField(). 
+	askTown.addEventListener("blur", townField);
+	askTown.addEventListener("keypress", townField);
+});
